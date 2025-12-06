2658bfc746da8a5fcdee545ac48460ab1ba441c5
pardeepsinghnegi <pardeepnegi7658@gmail.com>
2025-12-06 18:17:49 +0530

    home-page-setup-6-12-2025

---
+++ src/EditProfile.jsx
@@ -0,0 +1,5 @@
+const EditProfile = () => {
+  return <>this is for edit profile</>;
+};
+
+export default EditProfile;
--- src/assets/images.js
+++ src/assets/images.js
@@ -1,7 +1,8 @@
 import SIGNUP from "../assets/singup.png"
-
+import LOGO from "../assets/logo.png"
 const IMAGES = {
     SIGNUP,
+    LOGO
 };
 
 export default IMAGES;
