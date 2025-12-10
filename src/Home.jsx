2c8fbe726dfce4e4430c6678690275e539137706
Vanshika <vanshika@visionvivante.com>
2025-12-10 19:02:48 +0530

    chat-section-final

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,8 +1,9 @@
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 import { useNavigate, useParams } from "react-router-dom";
 import { firestore } from "./Firebase/Firebase";
-import { arrayUnion } from "firebase/firestore";
 import {
+  arrayUnion,
+  onSnapshot,
   addDoc,
   collection,
   getDocs,
@@ -16,30 +17,50 @@ import { Button, Upload } from "antd";
 import IMAGES from "./assets/images";
 
 const Home = () => {
+  const chatRef = useRef(null);
   const [user, setUserData] = useState({});
   const [data, setData] = useState([]);
   const [isChatSidebarOpen, setIsChatSidebarOpen] = useState(false);
   const [search, setSearch] = useState("");
+  const [message, setMessages] = useState({ messages: [] });
   const [sendMsg, setSendMsg] = useState("");
-  const [currentUser, setCurrentUser] = useState([])
-  console.log("current use is", currentUser)
+  const [currentUser, setCurrentUser] = useState([]);
+
   const navigate = useNavigate();
   const id = localStorage.getItem("uid");
   const { id: uidUrl } = useParams();
-  console.log(uidUrl)
+
   useEffect(() => {
     getCurrentUser();
     getUsers();
-  }, []);
+    if (chatRef.current) {
+      chatRef.current.scrollTop = chatRef.current.scrollHeight;
+    }
+  }, [message]);
 
 
-  console.log("ALL CHATS →", currentUser);
+  // Run when uid url will be available 
+  useEffect(() => {
+    if (!uidUrl) return;
+    const docRef = doc(firestore, "users", id, "chat", uidUrl);
+    const unsub = onSnapshot(docRef, (snap) => {
+      if (snap.exists()) {
+        setMessages(snap.data());
+      } else {
+        setMessages({ messages: null });
+      }
+    });
+
+    return () => unsub();
+  }, [uidUrl]);
+
 
   const getCurrentUser = async () => {
     try {
       const ref = doc(firestore, "users", id);
       const snap = await getDoc(ref);
       const userData = snap.data().values;
+
       const chatRef = collection(firestore, "users", id, "chat");
       const chatSnap = await getDocs(chatRef);
 
@@ -54,7 +75,6 @@ const Home = () => {
 
       setUserData(userData);
       setCurrentUser(chatList);
-
 
     } catch (err) {
       console.log("Error fetching user + chat:", err);
@@ -78,35 +98,26 @@ const Home = () => {
     }
   };
 
+
+  //  Set the messages in the firesote
   const selectChatUser = async (otherUser) => {
-    const uid = localStorage.getItem("uid");
+    let userMsg = sendMsg;
+    setSendMsg("");
+    if (!sendMsg.trim()) return;
     const otherUserId = otherUser?.id;
+    const myRef = doc(firestore, "users", id, "chat", otherUserId);
+    const theirRef = doc(firestore, "users", otherUserId, "chat", id);
 
     try {
-      const currentUserChatRef = doc(
-        firestore,
-        "users",
-        uid,
-        "chat",
-        otherUserId
-      );
-
-      const otherUserChatRef = doc(
-        firestore,
-        "users",
-        otherUserId,
-        "chat",
-        uid
-      );
-
       await setDoc(
-        currentUserChatRef,
+        myRef,
         {
           values: {
-            uidData: otherUser?.values, uid
+            uidData: otherUser.values,
+            uid: id
           },
           messages: arrayUnion({
-            send: sendMsg,
+            send: userMsg,
             time: Date.now()
           })
         },
@@ -114,26 +125,35 @@ const Home = () => {
       );
 
       await setDoc(
-        otherUserChatRef,
+        theirRef,
         {
           values: {
-            uidData: user, uid
+            uidData: user,
+            uid: otherUserId
           },
           messages: arrayUnion({
-            recieved: sendMsg,
+            recieved: userMsg,
             time: Date.now()
           })
         },
         { merge: true }
       );
 
-      setSendMsg("");
+
     } catch (err) {
-      console.log(err);
+      console.log("send error:", err);
     }
-  };
+  }
 
 
+  const formatTime = (ms) => {
+    const d = new Date(ms);
+    let hours = d.getHours();
+    let minutes = d.getMinutes();
+    if (hours > 12) hours -= 12;
+    if (hours === 0) hours = 12;
+    return `${hours}.${minutes.toString().padStart(2, "0")}`;
+  }
   return (
     <>
       <div className="container-xxl">
@@ -196,54 +216,54 @@ const Home = () => {
               </div>
 
               {data.map((item, idx) => (
-                <div
-                  onClick={() => navigate(`/chat/${item?.id}`)}
-                  className="user-container brown pointer"
-                  key={idx}
-                >
-                  <div className="messangers mt-2 align-items-center">
-                    <div className="image">
-                      <img
-                        src={item?.values?.image}
-                        alt="user-image"
-                        className="img-fluid rounded-circle"
-                      />
-                    </div>
+                item?.id === localStorage.getItem("uid") ? null :
+                  <div
+                    onClick={() => navigate(`/chat/${item?.id}`)}
+                    className="user-container brown pointer"
+                    key={idx}
+                  >
+                    <div className="messangers mt-2 align-items-center">
+                      <div className="image">
+                        <img
+                          src={item?.values?.image}
+                          alt="user-image"
+                          className="img-fluid rounded-circle"
+                        />
+                      </div>
 
-                    <div className="d-flex justify-content-between align-items-center w-100">
-                      <span className="w-75">
-                        <h5 className="mb-1 fs-6">{item?.values?.name}</h5>
-                        <p className="m-0 one-line w-75 text-secondary">
-                          Last message preview...
-                        </p>
-                      </span>
+                      <div className="d-flex justify-content-between align-items-center w-100">
+                        <span className="w-75">
+                          <h5 className="mb-1 fs-6">{item?.values?.name}</h5>
+                          <p className="m-0 one-line w-75 text-secondary">
+                            Last message preview...
+                          </p>
+                        </span>
 
-                      <span className="d-flex flex-column justify-content-center ">
-                        <h6
-                          className="m-1 text-secondary fw-semibold"
-                          style={{ fontSize: "14px" }}
-                        >
-                          09:15AM
-                        </h6>
-                        <p className="ms-btn mb-0">4</p>
-                      </span>
+                        <span className="d-flex flex-column justify-content-center ">
+                          <h6
+                            className="m-1 text-secondary fw-semibold"
+                            style={{ fontSize: "14px" }}
+                          >
+                            09:15AM
+                          </h6>
+                          <p className="ms-btn mb-0">4</p>
+                        </span>
+                      </div>
                     </div>
                   </div>
-                </div>
+
               ))}
             </div>
           </div>
 
           {/* CHAT SECTION */}
+
           {data.map((item, idx) =>
             uidUrl === item?.id ? (
-              <div
-                className=" col-md-6 p-0 border-2 position-relative"
-                key={idx}
-                style={{ height: "100vh" }}
-              >
+              <div className=" col-md-6 border-2 position-relative chat-section" key={idx} u>
                 <div className="container-fluid">
                   <div className="row gap-2">
+
                     <div className="col-12 py-2 d-flex justify-content-around align-items-center brown d-none d-md-block">
                       <div className="mt-2 d-flex gap-2 align-items-center">
                         <div className="online-user-image">
@@ -259,7 +279,7 @@ const Home = () => {
                             <p className="mb-0 fw-bolder">
                               {item.values?.name}
                             </p>
-                            <p className="m-0 fw-bold text-primary">Online</p>
+                            <p className="mb-0 online">Online</p>
 
                           </div>
                           <p className="mb-0 fst-normal text-secondary">
@@ -269,82 +289,67 @@ const Home = () => {
                       </div>
                     </div>
 
-                    {/* MESSAGES DUMMY AREA */}
-                    <div className="col-12 h-100 d-flex flex-column gap-3 my-2">
-                      {currentUser?.map((item, index) => (
-                        // Check if this chat belongs to the opened user
-                        item?.values?.uid === uidUrl ? (
-                          <div key={index}>
-                            {item?.messages?.map((msg, idx) => (
-                              <React.Fragment key={idx}>
-                                {msg?.received && (
-                                  <p className="msg-scond-prson">
-                                    {msg.received}
-                                  </p>
-                                )}
+                    {/* MESSAGES LIST */}
+                    <div className="col-12 coooot position-relative  pt-3" ref={chatRef}>
+                      {message?.messages?.map((value, index) => (
+                        <div key={index} className="row p-0 m-0">
+                          {value?.recieved && (
+                            <div className="col-6 mb-1">
+                              <p className=" msg-scond-prson  d-flex flex-column gap-1">{value.recieved}
+                                <span className="time">{formatTime(value?.time)}</span>
+                              </p>
+                            </div>
+                          )}
+                          {value?.send && (
+                            <div className="row justify-content-end p-0 m-0">
+                              <span className="col-6 mb-1 right">
+                                <p className="msg-user d-flex flex-column justify-content-end">{value.send}
+                                  <span className="time">{formatTime(value?.time)}</span></p>
+                              </span>
+                            </div>
+                          )}
+                        </div>
 
-                                {msg?.send && (
-                                  <p className="msg-user text-end">
-                                    {msg.send}
-                                  </p>
-                                )}
-                              </React.Fragment>
-                            ))}
-                          </div>
-                        ) : null
                       ))}
+
                     </div>
 
+                  </div>
+                </div>
+                {/* MESSAGE INPUT */}
+                <div className="chat-section-input">
+                  <div className="d-flex justify-content-center  align-items-center">
+                    <Upload>
+                      <Button className="border-0">
+                        <img src={IMAGES.CLIP} alt="" className="img-fluid" />
+                      </Button>
+                    </Upload>
 
-
-                    {/* MESSAGE INPUT */}
-                    <div
-                      className="col-12 position-absolute bottom-0 brown py-4"
-                      style={{ width: "100%" }}
-                    >
-                      <div className="d-flex justify-content-center gap-1 align-items-center">
-                        <Upload>
-                          <Button className="border-0">
-                            <img
-                              src={IMAGES.CLIP}
-                              alt=""
-                              className="img-fluid"
-                            />
-                          </Button>
-                        </Upload>
-
-                        <form className="w-100 d-flex">
-                          <div className="position-relative send-input">
-                            <Input
-                              value={sendMsg}
-                              onChange={(e) => setSendMsg(e.target.value)}
-                              className="py-2 fw-bold border-secondary rounded-pill"
-                              size="large"
-                              placeholder="Enter message"
-                            />
-                            <button
-                              className="send-img"
-                              onClick={(e) => {
-                                e.preventDefault();
-                                selectChatUser(item);
-                              }}
-                            >
-                              <img
-                                src={IMAGES.SEND}
-                                className="img-fluid"
-                              />
-                            </button>
-                          </div>
-                        </form>
+                    <form className="w-100 d-flex">
+                      <div className="position-relative send-input">
+                        <Input
+                          value={sendMsg}
+                          onChange={(e) => setSendMsg(e.target.value)}
+                          className="py-2 fw-bold border-secondary rounded-pill"
+                          size="large"
+                          placeholder="Enter message"
+                        />
+                        <button
+                          className="send-img"
+                          onClick={(e) => {
+                            e.preventDefault();
+                            selectChatUser(item);
+                          }}
+                        >
+                          <img src={IMAGES.SEND} className="img-fluid" />
+                        </button>
                       </div>
-                    </div>
+                    </form>
                   </div>
                 </div>
               </div>
             ) : null
           )}
-
-
           < div className={` d-block d-md-none chat-sidebar m-0 ${isChatSidebarOpen ? "open" : ""}`}>
             <div className="d-flex justify-content-end">
               <button
@@ -389,8 +394,10 @@ const Home = () => {
               </div>
             ))}
           </div >
+
+
         </div>
-      </div>
+      </div >
     </>
   );
 };
